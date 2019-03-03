ea6bc94721a349b29dd10343489224741e145972
Rothanak So <rothanak.hello@gmail.com>
2019-03-03 09:40:43 -0500

    Push Cloud Firestore integration

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,23 @@
-const functions = require('firebase-functions');
 const express = require('express');
 const bodyParser = require('body-parser');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
+// Initialize Firestore
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+const db = admin.firestore();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Import routes
-const indexRoutes = require('./routes/index');
-const itemRoutes = require('./routes/item');
+// const indexRoutes = require('./routes/index');
+const itemRoutes = require('./routes/item.js')(db);
 
-app.use('/', indexRoutes);
+// app.use('/', indexRoutes);
 app.use('/items', itemRoutes);
 
 exports.app = functions.https.onRequest(app);
--- functions/routes/item.js
+++ functions/routes/item.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
 
-router.post('/new', (req, res) => {
-  res.json({
-    status: 'API Its working',
-    message: 'Welcome to Marketplace!',
+module.exports = (db) => {
+  // Create a new listing
+  router.post('/new', (req, res) => {
+    db.collection('items').add(req.body)
+      .then(docRef => res.status(200).send(docRef.id))
+      .catch(err => res.status(500).send(err));
   });
-});
 
-module.exports = router;
+  // Get all listings
+  router.get('/', (_req, res) => {
+    db.collection('items').get()
+      .then(snapshot => (snapshot.size > 0
+        ? res.status(200).json(snapshot.docs.map(it => it.data()))
+        : res.status(404).send('No listings found!')))
+      .catch(err => res.status(500).send(err));
+  });
+
+  // Get a specific listing
+  // router.get('/:id', (req, res) => {
+  // });
+
+  return router;
+};
